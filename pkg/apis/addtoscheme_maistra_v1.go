567dc4815b57f34800305b9ee0c1e43cb5702bde
Marko Lukša <marko.luksa@gmail.com>
2020-01-09 14:59:46 +0100

    Temporary fix for missing authentication.maistra.io/v1 resources

--- pkg/apis/addtoscheme_maistra_v1.go
+++ pkg/apis/addtoscheme_maistra_v1.go
@@ -1,7 +1,7 @@
 package apis
 
 import (
-	v1 "github.com/maistra/istio-operator/pkg/apis/maistra/v1"
+	maistrav1 "github.com/maistra/istio-operator/pkg/apis/maistra/v1"
 
 	networkv1 "github.com/openshift/api/network/v1"
 	routev1 "github.com/openshift/api/route/v1"
@@ -11,7 +11,8 @@ import (
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
-		v1.SchemeBuilder.AddToScheme,
+		maistrav1.SchemeBuilder.AddToScheme,
+		Authentication_AddToScheme,
 		networkv1.AddToScheme,
 		routev1.AddToScheme,
 		k8score.AddToScheme)
